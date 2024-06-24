f4a54f754877e9a182c6a5eed73060bebc161155
ARENA NICOLAS <nicolas.arena2@etu.unistra.fr>
2024-06-24 15:58:43 +0200

    layoutV2

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,22 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "ARENA Nicolas | Portfolio",
   description: "ARENA Nicolas, un développeur fullstack passionné par la création de sites web et d'applications web modernes et performantes.",
+  url: "https://nguyen-minh.dev",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+  keywords: [
+      "Creative Web Developer France",
+      "Web Developer Portfolio",
+      "Next.js Portfolio",
+      "React Portfolio",
+      "Arena Nicolas Web Apps",
+      "Fullstack Developer",
+      "Creative Developer",
+      "Creative Developer Strasbourg",
+      "Next.js portfolio",
+      "React portfolio",
+      "arena nicolas portfolio",
+  ],
+  image: "/images/computer.jpg",
 };
 
 export default function RootLayout({
